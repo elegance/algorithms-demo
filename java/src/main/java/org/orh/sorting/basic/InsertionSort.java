b45a19ba0153b169a0bcdb9be35c3a6a11a7c2b0
elegance <webdevelop@sohu.com>
2017-10-12 14:20:15 +0800

    插入排序的实现与测试

---
+++ java/src/main/java/org/orh/sorting/basic/InsertionSort.java
@@ -0,0 +1,50 @@
+package org.orh.sorting.basic;
+
+import java.util.Arrays;
+
+import org.orh.sorting.SortTestHelper;
+import org.orh.sorting.basic.ext.SelectionSortObject;
+
+/**
+ * 插入排序: 插扑克牌，从第二张排开始往前依次比较，如果小则交换，否则可以从下一张牌继续比较
+ *
+ */
+public class InsertionSort {
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public static void sort(Comparable[] arr) {
+        for (int i = 1, len = arr.length; i < len; i++) {
+
+            for (int j = i; j > 0; j--) { // 往前比较
+                if (arr[j].compareTo(arr[j - 1]) < 0) { // 挨个往前比，比前者小则交换
+                    swap(arr, j, j - 1);
+                }
+            }
+        }
+    }
+
+    @SuppressWarnings("rawtypes")
+    public static void swap(Comparable[] arr, int i, int j) {
+        Comparable tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        // 使用同样的数据集合 来对比 插入排序、选中排序
+        Integer[] inArr = SortTestHelper.generateRandomArray(10000, 0, 100);
+        Integer[] seArr = Arrays.copyOf(inArr, inArr.length);
+
+        // ☆☆☆ 百思不得其解 ：（JDK版本：jdk1.8.0_141）
+        // 1. 如果按以下代码47,48行， 先执行"插入排序"，再执行 "选择排序" ，则 “选择排序”的速度会慢出许多
+        // 2. 如果调换下47,48 行，则"选择排序"的速度会比原来 快上3-4倍
+        
+        // 已经排除以下可能，做了以下尝试，但结果还是一样：
+        // 1. 不采用 Array.copyOf，两个方法都采用 全新生成的 数组
+        // 2. 不使用System.out.print ，使用Logger打印
+        // 3. 不使用jdk8 的 lambda Consumer方法，使用直接调用静态方法
+        
+        SortTestHelper.testSort("InsectionSort", InsertionSort::sort, inArr);
+        SortTestHelper.testSort("SelectionSort", SelectionSortObject::sort, seArr);
+    }
+}
--- readme.md
+++ readme.md
@@ -8,3 +8,11 @@
         - 取最小要遍历比较，故不可提前结束
     * 代码：
         - [java选择排序的实现](.\java\src\main\java\org\orh\sorting\basic\SelectionSort.java)
+        - [java选择排序泛型支持](.\java\src\main\java\org\orh\sorting\basic\ext\SelectionSortObject.java)
+
+2. 插入排序，记忆口诀：**挨个往前比较插入** (往前比较，意味着循环开始从第二个元素开始)
+    * 特点：
+        - 挨个比较不满足条件时，可**提前退出循环**
+        - 挨个比较插入，发生多次swap (一次 swap = 3次赋值)
+    * 代码：
+        - [java插入排序的实现](.\java\src\main\java\org\orh\sorting\basic\InsertionSort.java)
