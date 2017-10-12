910fab4965cda12081110b6faa19e32d560ac4c8
elegance <webdevelop@sohu.com>
2017-10-12 10:09:01 +0800

    排序测试辅助类增强

--- java/src/main/java/org/orh/sorting/SortTestHelper.java
+++ java/src/main/java/org/orh/sorting/SortTestHelper.java
@@ -1,5 +1,10 @@
 package org.orh.sorting;
 
+import java.time.Duration;
+import java.util.function.Consumer;
+
+import org.orh.sorting.basic.ext.SelectionSortObject;
+
 /**
  * 排序测试帮助类
  *
@@ -7,7 +12,7 @@ package org.orh.sorting;
 public class SortTestHelper {
 
     /**
-     * 随机生成指定长度的整数数组  [rangeL, rangeR]
+     * 随机生成指定长度的整数数组 [rangeL, rangeR]
      * 
      * @param size 需要生成的元素个数
      * @param rangeL 区间开始值
@@ -28,6 +33,39 @@ public class SortTestHelper {
         }
         return arr;
     }
-    
+
     // 可以fork/join 分段 来优化大数组的初始化
+
+    /**
+     * 测试排序
+     * 
+     * @param consumer 因为排序方法是无返回值的，所以此处使用 Consumer<T>，如果有返回值可以使用 Function<T, R>
+     * @param arr
+     */
+    @SuppressWarnings("rawtypes")
+    public static void testSort(String sortName, Consumer<Comparable[]> sortFn, Comparable[] arr) {
+        long start = System.currentTimeMillis();
+        sortFn.accept(arr);
+        long end = System.currentTimeMillis();
+
+        assert isSorted(arr);
+
+        Duration consumptionTime = Duration.ofMillis(end - start); // 便于单位转换，虽然暂时没用到转换
+
+        System.out.printf("%-20s: sort %d elements take %d ms\n", sortName, arr.length, consumptionTime.toMillis());
+    }
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public static boolean isSorted(Comparable[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i].compareTo(arr[i - 1]) < 0) { // 后者比前者小则返回false
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        SortTestHelper.testSort("SelectionSort", SelectionSortObject::sort, SortTestHelper.generateRandomArray(1000, 0, 100));
+    }
 }
